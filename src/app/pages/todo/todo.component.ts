81f3e05675c00e7ce2f0f5abf5ede85e47a9e64e
KAME425 <zkamekyu@gmail.com>
2023-07-30 17:30:24 +0000

    Improved ToDo-List

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -19,10 +19,16 @@ export class TodoComponent {
     this.taskState[index] = $event;
   }
 
-  public onSubmit(form: NgForm) {
-    this.todoList.push(form.value.todoInput);
-    form.reset();
-
+  public deleteEntry(i: number) {
+    this.todoList = this.todoList.filter((v, i2) => i != i2)
     localStorage.setItem('todoList', JSON.stringify(this.todoList));
+  }
+
+  public onSubmit(form: NgForm) {
+    if (form.value.todoInput.split(" ").length > 0 && form.value.todoInput) {
+      this.todoList.push(form.value.todoInput);
+      form.reset();
+      localStorage.setItem('todoList', JSON.stringify(this.todoList));
+    }
   }
 }
